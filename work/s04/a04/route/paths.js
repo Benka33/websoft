bb1aa5cdf94155b1e4cdcc56deab54429dcbd2cd
Filip Bengtsson <filip.bengtsson@hotmail.com>
2020-02-07 11:25:53 +0100

    a04 removed duplicity in lotto

--- work/s04/a04/route/paths.js
+++ work/s04/a04/route/paths.js
@@ -18,40 +18,33 @@ router.get("/about", (req, res) => {
 
 // Add a route for the path /lotto
 router.get("/lotto", (req, res) => {
-    //for(let counter = 1; counter < 8; counter++){
-        var rand1 = (Math.random()*35) +1;
-        rand1 = parseInt(rand1);
-        var rand2 = (Math.random()*35) +1;
-        rand2 = parseInt(rand2);
-        var rand3 = (Math.random()*35) +1;
-        rand3 = parseInt(rand3);
-        var rand4 = (Math.random()*35) +1;
-        rand4 = parseInt(rand4);
-        var rand5 = (Math.random()*35) +1;
-        rand5 = parseInt(rand5);
-        var rand6 = (Math.random()*35) +1;
-        rand6 = parseInt(rand6);
-        var rand7 = (Math.random()*35) +1;
-        rand7 = parseInt(rand7);
-    //}
- 
-    var lottery = "<!DOCTYPE html> <html> <head> <style> table, th, td { border: 1px solid black; border-collapse: collapse; }"+
-     "</style> </head> <body> <caption>The Lottery!</caption> <table style="+"width:100%"+ "> <tr>";
-    
+
+        var lottery = "<!DOCTYPE html> <html> <head> <style> table, th, td { border: 1px solid black; border-collapse: collapse; }"+
+        "</style> </head> <body> <caption>The Lottery!</caption> <table style="+"width:100%"+ "> <tr>";
+
+        var randArray = [];
+        for(let i = 0; i < 7; i++){
+            while(true){
+                var nbr = Math.floor(Math.random()*35) +1;
+                if(!(randArray.includes(nbr))){
+                    randArray[i] = nbr;
+                    console.log(randArray[i]);
+                    break;
+                }
+            }
+        }
+        
     for(let i = 1; i < 36; i++) {
        
-        if(i == rand1 || i == rand2 || i == rand3 || i == rand4 || i == rand5 || i == rand6 || i == rand7){
+        if(randArray.includes(i)){
         lottery += "<td bgcolor="+"grey"+">"+i+"</td>"
         } else {
             lottery += "<td>"+i+"</td>" 
         }
 
-        if (i == 35){
-            lottery += "</tr> </table> </body> </html>"
-        }
     }
+    lottery += "</tr> </table> </body> </html>";
 
-    //res.send("<h1>The Lottery!</h1>");
     res.send(lottery)
 
     
